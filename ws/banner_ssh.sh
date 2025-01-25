6931b5e5929e01dede5e7038e1b7345a8924832a
GeKaStore <khairilsofia2@gmail.com>
2025-01-25 08:16:36 +0800

    Add files via upload

---
+++ ws/banner_ssh.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+cat > /etc/issue.net << END
+<font color="green">Nikmatin Kang....</font>
+<p style="text-align: center;">
+<font color="cyan"><b><big><big><big>WUZZSTORE</font></b></big></big></big></p>
+<p style="text-align: center;">
+    <span style="color: #7FFF00;">Support us:</span>
+    <span style="color: #EFEFEF;">WA: wa.me/6287760204418</span>
+    <span style="color: #EFEFEF;">TL: t.me/WuzzSTORE</span>
+</p>
+END
